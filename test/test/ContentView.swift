787f07560e4895831cfb9147dc1d4ce180317677
YUHASUNG <yuha822@naver.com>
2024-07-10 21:17:46 +0900

    SwitftUI View에 대한 설명

--- test/test/ContentView.swift
+++ test/test/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    // View : 한 개 이상의 View를 묶어서 배치할 때 사용할 수 있는 것 중에 하나
     var body: some View {
-        VStack { // VStack함수
+        VStack { // VStack함수 : View를 "수직"으로 배치해주는 View
             Rectangle()
                 .frame(width: 50, height: 50)
                 .foregroundStyle(.teal)
-            HStack { // HStack함수
+            HStack { // HStack함수 : View를 "수평"으로 배치해주는 View
                 Rectangle()
                     .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
                     .foregroundStyle(.green)
